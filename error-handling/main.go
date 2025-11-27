a9a19a623ce402e7f6b6a035fcab19486a5c167f
Edwin Boon <hello@edwinboon.dev>
2025-11-27 15:28:59 +0100

    Add some basic error handling

--- error-handling/main.go
+++ error-handling/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 type Truck struct {
 	id string
 }
 
+func (t *Truck) LoadCargo() error {
+	return nil
+}
+
+func (t *Truck) UnloadCargo() error {
+	return nil
+}
+
 // processTruck handles the loading and unloading of a truck.
-func processTruck(truck Truck) {
+func processTruck(truck Truck) error {
 	fmt.Printf("Processing truck: %s\n", truck.id)
+
+	if err := truck.LoadCargo(); err != nil {
+		return fmt.Errorf("error loading cargo for truck %s: %w", truck.id, err)
+	}
+
+	if err := truck.UnloadCargo(); err != nil {
+		return fmt.Errorf("error unloading cargo for truck %s: %w", truck.id, err)
+	}
+
+	return nil
 }
 
 func main() {
@@ -22,6 +41,10 @@ func main() {
 
 	for _, truck := range trucks {
 		fmt.Printf("Truck %s arrived.\n", truck.id)
-		processTruck(truck)
+
+		err := processTruck(truck)
+		if err != nil {
+			log.Fatalf("Error processing truck: %s", err)
+		}
 	}
 }
