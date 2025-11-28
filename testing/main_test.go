47892dca5e7dee406ee6c33a9f8322fa0485d1df
Edwin Boon <hello@edwinboon.dev>
2025-11-28 08:47:41 +0100

    setup test structure

---
+++ testing/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	t.Run("processTruck", func(t *testing.T) {
+		t.Run("should load and unload a trucks cargo without errors", func(t *testing.T) {})
+	})
+}
