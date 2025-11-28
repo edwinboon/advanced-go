b14829ad04bf08a137118547521d4d4c60ad52ac
Edwin Boon <hello@edwinboon.dev>
2025-11-28 08:43:07 +0100

    Improved code by using a interface for Truck

--- interfaces/main.go
+++ interfaces/main.go
@@ -5,46 +5,74 @@ import (
 	"log"
 )
 
-type Truck struct {
-	id string
+type Truck interface {
+	LoadCargo() error
+	UnloadCargo() error
 }
 
-func (t *Truck) LoadCargo() error {
+type NormalTruck struct {
+	id    string
+	cargo int
+}
+
+type ElectricTruck struct {
+	id           string
+	cargo        int
+	batteryLevel float64
+}
+
+func (t *NormalTruck) LoadCargo() error {
+	t.cargo += 100
 	return nil
 }
 
-func (t *Truck) UnloadCargo() error {
+func (t *NormalTruck) UnloadCargo() error {
+	t.cargo = 0
+	return nil
+}
+
+func (e *ElectricTruck) LoadCargo() error {
+	e.cargo += 100
+	e.batteryLevel -= 10.0
+	return nil
+}
+
+func (e *ElectricTruck) UnloadCargo() error {
+	e.cargo = 0
+	e.batteryLevel -= 5.0
 	return nil
 }
 
 // processTruck handles the loading and unloading of a truck.
 func processTruck(truck Truck) error {
-	fmt.Printf("Processing truck: %s\n", truck.id)
+	fmt.Printf("processing truck %+v\n", truck)
 
 	if err := truck.LoadCargo(); err != nil {
-		return fmt.Errorf("error loading cargo for truck %s: %w", truck.id, err)
+		return fmt.Errorf("error loading cargo for truck: %w", err)
 	}
 
 	if err := truck.UnloadCargo(); err != nil {
-		return fmt.Errorf("error unloading cargo for truck %s: %w", truck.id, err)
+		return fmt.Errorf("error unloading cargo for truck: %w", err)
 	}
 
 	return nil
 }
 
 func main() {
-	trucks := []Truck{
-		{id: "Truck-1"},
-		{id: "Truck-2"},
-		{id: "Truck-3"},
+	nt := &NormalTruck{id: "Truck-1", cargo: 0}
+	et := &ElectricTruck{id: "ETruck-1", cargo: 0, batteryLevel: 100.0}
+
+	err := processTruck(nt)
+	if err != nil {
+		log.Fatalf("Error processing normal truck: %s", err)
 	}
 
-	for _, truck := range trucks {
-		fmt.Printf("Truck %s arrived.\n", truck.id)
-
-		err := processTruck(truck)
-		if err != nil {
-			log.Fatalf("Error processing truck: %s", err)
-		}
+	err = processTruck(et)
+	if err != nil {
+		log.Fatalf("Error processing normal ETruck: %s", err)
 	}
+
+	log.Printf("Normal Truck after processing: %+v\n", nt)
+	log.Printf("Electric Truck after processing: %+v\n", et)
+	log.Printf("All trucks processed successfully.")
 }
