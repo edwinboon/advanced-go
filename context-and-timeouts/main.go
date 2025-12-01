90ca29c0ac0aff2386261dd3a2c457c0139918de
Edwin Boon <hello@edwinboon.dev>
2025-12-01 10:49:29 +0100

    finished the lesson about context and timeouts

--- context-and-timeouts/main.go
+++ context-and-timeouts/main.go
@@ -7,6 +7,10 @@ import (
 	"time"
 )
 
+type ContextKey string
+
+var UserIDKey ContextKey = "userID"
+
 type Truck interface {
 	LoadCargo() error
 	UnloadCargo() error
@@ -46,8 +50,24 @@ func (e *ElectricTruck) UnloadCargo() error {
 }
 
 // processTruck handles the loading and unloading of a truck.
-func processTruck(truck Truck) error {
+func processTruck(ctx context.Context, truck Truck) error {
 	fmt.Printf("processing truck %+v\n", truck)
+
+	// access the userId from context
+	// userID := ctx.Value(UserIDKey)
+	// fmt.Printf("User ID from context: %v\n", userID)
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*2)
+	defer cancel()
+
+	// simulate a long running process
+	delay := time.Second * 5 // 1
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case <-time.After(delay):
+		break
+	}
 
 	// simulate some processing time
 	time.Sleep(time.Second * 1)
@@ -68,14 +88,14 @@ func processTruck(truck Truck) error {
  * only add the keyword 'go' will not work as expected because the main function may exit before goroutines complete.
  * there fore, we need to add a waitGroup mechanism to ensure all goroutines finish before main exits.
  */
-func processFleet(trucks []Truck) error {
+func processFleet(ctx context.Context, trucks []Truck) error {
 	var wg sync.WaitGroup
 
 	for _, t := range trucks {
 		wg.Add(1) // wait for 1 goroutine each iteration
 
 		go func(t Truck) {
-			if err := processTruck(t); err != nil {
+			if err := processTruck(ctx, t); err != nil {
 				fmt.Printf("Error processing truck: %s\n", err)
 			}
 			wg.Done()
@@ -89,8 +109,9 @@ func processFleet(trucks []Truck) error {
 
 func main() {
 	ctx := context.Background()
+	ctx = context.WithValue(ctx, UserIDKey, 42)
 
-	Fleet := []Truck{
+	fleet := []Truck{
 		&NormalTruck{id: "NT1", cargo: 0},
 		&ElectricTruck{id: "ET1", cargo: 0, batteryLevel: 100},
 		&NormalTruck{id: "NT2", cargo: 0},
@@ -98,7 +119,7 @@ func main() {
 	}
 
 	// Process all trucks concurrently
-	if err := processFleet(Fleet); err != nil {
+	if err := processFleet(ctx, fleet); err != nil {
 		fmt.Printf("Error processing fleed: %v\n", err)
 		return
 	}
